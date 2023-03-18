48b19cc5d4ebde61e0d20d70b43bf6571e50f2fe
Maarten Hogeveen <maarten@maartens-mbp.lan>
2023-03-18 10:37:04 +0100

    feat: add image domain permission

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['eldenring.wiki.fextralife.com'],
+  },
 }
 
 module.exports = nextConfig
