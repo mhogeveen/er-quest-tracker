1739e183542fbdf89d84cdf474a8d4a03b483680
Maarten Hogeveen <mhogeveen94@gmail.com>
2023-04-10 14:43:01 +0200

    refactor: reduce checkbox complexity

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,28 +1,21 @@
 import { CircleIcon, CheckCircledIcon } from '@radix-ui/react-icons'
 import styles from './Checkbox.module.scss'
-import { useState, MouseEvent, useEffect } from 'react'
+import { MouseEvent } from 'react'
 
 type CheckboxProps = {
   isChecked?: boolean
   onClick?: (state: boolean) => void
 }
 
-export const Checkbox = ({ isChecked = false, onClick }: CheckboxProps) => {
-  const [checked, setChecked] = useState(false)
-
-  useEffect(() => {
-    setChecked(isChecked)
-  }, [isChecked])
-
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+export const Checkbox = ({ isChecked, onClick }: CheckboxProps) => {
+  const handleClick = (e: MouseEvent) => {
     e.preventDefault()
-    onClick?.(!checked)
-    setChecked((prev) => !prev)
+    onClick?.(!isChecked)
   }
 
   return (
     <button className={styles.checkbox} onClick={handleClick}>
-      {checked ? (
+      {isChecked ? (
         <CheckCircledIcon width={20} height={20} />
       ) : (
         <CircleIcon width={20} height={20} />
