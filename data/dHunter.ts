99333217f2f8c322575c37be1e40202969f5998d
Maarten Hogeveen <mhogeveen94@gmail.com>
2024-03-09 12:07:54 +0100

    feat: finalize dHunter npc data

--- data/dHunter.ts
+++ data/dHunter.ts
@@ -25,10 +25,46 @@ export const dHunter: Npc = {
   steps: [
     {
       id: 1,
-      optional: false,
-      description: '',
+      optional: true,
+      description:
+        'Find D to the west of [Summonwater Village](https://eldenring.wiki.fextralife.com/Summonwater+Village) to the side of the path.',
       zone: Zone.LIMGRAVE,
-      note: '',
+      note: 'If you defeat Godrick or the Red Wolf of Radagon before talking to D in Limgrave, he can be found at Roundtable Hold.',
+    },
+    {
+      id: 2,
+      optional: false,
+      description:
+        "Defeat the Tibia Mariner in Summonwater Village and exhaust D's dialogue at either the Roundtable Hold or the entrace to Summonwater Village.",
+      zone: Zone.LIMGRAVE,
+    },
+    {
+      id: 3,
+      optional: false,
+      description:
+        'Visit [Gurranq](https://eldenring.wiki.fextralife.com/Gurranq+Beast+Clergyman) at the [Bestial Sanctum](https://eldenring.wiki.fextralife.com/Bestial+Sanctum) in Caelid.',
+      zone: Zone.CAELID,
+    },
+    {
+      id: 4,
+      optional: true,
+      description:
+        'After speaking to Gurranq you will be able to buy incantations from D.',
+      zone: Zone.ROUNDTABLE_HOLD,
+    },
+    {
+      id: 5,
+      optional: true,
+      description:
+        "Follow Rogier and Fia's quests or kill D at the first step to acquire his armor.",
+      zone: Zone.LANDS_BETWEEN,
+    },
+    {
+      id: 6,
+      optional: true,
+      description: "Find D's brother in Nokron and give him D's armor.",
+      zone: Zone.NOKRON,
+      note: "The outcome of this step depends on whether the armor was obtained by killing D or via Rogier and Fia's quests.",
     },
   ],
   failureConditions: ['D dies when you reach Farum Azula'],
